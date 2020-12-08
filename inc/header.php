e0b30b577ed1f500200d76b34e1161c40d4c45f5
Luis Eduardo Mori Ayala <ing.moriayala@gmail.com>
2020-12-07 20:46:06 -0500

    a

--- inc/header.php
+++ inc/header.php
@@ -39,8 +39,51 @@
   js.src = 'https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v3.0';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- menu categorias -->
+<div class=" position-left off-canvas-absolute menu_canvas" id="menucateg" data-off-canvas>
+<?php 
+	$categ=executesql("select * from categorias where estado_idestado=1 order by orden desc");
+	if(!empty($categ)){ ?>
+	<!--
+	<ul class="vertical menu drilldown" data-drilldown data-scroll-top="true" data-auto-height="true" data-animate-height="true" >
+	-->
+	<ul class=" no-bullet"  >
+		<?php 
+			foreach($categ as $categoria){ 
+				$active = ($categoria["nombre_rewrite"]== $pagina)?'active-menu':''; 
+		?>
+			<li class="<?php echo $active;?>">
+					<a href="productos/<?php echo $categoria["nombre_rewrite"]; ?>"><?php echo $categoria["nombre"]; ?></a>
+					<?php 
+						// muestro menu desplegable de subacategorias de categ destacadas 
+						$sql_sub="select * from subcategorias where estado_idestado=1 and idcat='".$categoria['idcat']."' order by orden desc  ";
+						$subcategorias=executesql($sql_sub); 
+						if(!empty($subcategorias)){ ?>
+						
+					<ul class="no-bullet">
+						<?php foreach($subcategorias as $subcateg){
+								$active = ($subcateg["nombre_rewrite"]== $pagina)?'active-menu':''; ?>
+									<li class="<?php echo $active;?>"><a href="productos/<?php echo $categoria["nombre_rewrite"].'/'.$subcateg["nombre_rewrite"]; ?>"><?php echo $subcateg["nombre"]; ?></a></li>				
+						<?php	} ?>						
+          </ul>
+					<?php	} ?>						
+					
+			</li>				
+	<?php	} ?>						
+	</ul>	
+<?php	} ?>	
+			
+			
+
+
+			
+</div>
+<!-- menu categorias -->
+
+
 <!-- menu responsive "left"  -->
-<div class="block-n position-left off-canvas-absolute " id="offCanvasLeftSplit1" data-off-canvas>
+<div class="block-n position-left off-canvas-absolute menu_canvas" id="offCanvasLeftSplit1" data-off-canvas>
 <?php if(!empty($_SESSION["suscritos"]["id_suscrito"])){ ?>
   <div class="fondo banner-1">
     <img class="img-perfil-movi" src="<?php echo $image_perfil;?>">
@@ -49,7 +92,7 @@
   </div>
 <?php } ?>  
   <nav> 
-    <ul class="no-bullet osans bold fullwidth">
+    <ul class="no-bullet gulim bold fullwidth">
 <?php 
 /*
 if(empty($_SESSION["suscritos"]["id_suscrito"])){ ?>
@@ -78,7 +121,9 @@ if(empty($_SESSION["suscritos"]["id_suscrito"])){ ?>
 									<?php	} ?>						
 								</ul>
 							</li>	
-					<?php	} ?>	
+					<?php	} 
+					
+					?>	
 						
 						<li class="<?php echo ("blog"== $pagina)?'active-menu':''; ?>"><a href="blog">BLOG </a></li>  
 						<li class="<?php echo ("contacto"== $pagina)?'active-menu':''; ?>"><a href="contacto">CONTÁCTENOS </a></li>  
@@ -107,21 +152,26 @@ if(empty($_SESSION["suscritos"]["id_suscrito"])){ ?>
     <li class="aparecer" style="padding-right:18px;"><a href="cesta"><img src="img/iconos/cesta.png"><span class="em color-1 monto_total" style="font-size:16px;">(s/ 350)</span></a></li>   
 -->		
     <div class="float-right ocultar">
-			 <div class="large-12 columns anexo float-right">
-						<div id="search"><div class="well monset">
-							<form action="productos" method="post" enctype="multipart/form-data"><fieldset style="position:relative;">
-								<div class="abs"><button class="btn-default"><img src="img/iconos/lupa.svg"></button></div>
-								<input autocomplete="off" class="form-control" type="text" name="buscar" required placeholder="Buscar ..">
-							</fieldset></form>
-						</div></div>
-						<li class="sinn" ><a href=""><img src="img/iconos/tel2020.png" style="padding-right:7px;">945250434 | 945250434</a></li>            
+		 <div class="large-12 columns anexo float-right"> <!-- sino poner afuera del nav -->
+					<div id="search"><div class="well monset">
+						<form action="productos" method="post" enctype="multipart/form-data"><fieldset style="position:relative;">
+							<div class="abs"><button class="btn-default"><img src="img/iconos/lupa.svg"></button></div>
+							<input autocomplete="off" class="form-control" type="text" name="buscar" required placeholder="Buscar ..">
+						</fieldset></form>
+					</div></div>
+					<li class="sinn" ><a href=""><img src="img/iconos/tel2020.png" style="padding-right:7px;">945250434 | 945250434</a></li>      
+					<!--
+					-->						
 			</div>
       <nav  class="hide amatic bold"> 
+				<button type="button" class="gulim bold  menu_bar menucateg" data-toggle="menucateg">
+					<p style="font-size:15px;">PRODUCTOS <img src="img/iconos/menu_movil.png" class=""></p>
+				</button>
         <ul class="no-bullet fullwidth">
 						<li class="<?php echo ("index"== $pagina)?'active-menu':''; ?>"><a href="<?php echo $url;?>">INICIO </a></li>  
 						<li class="<?php echo ("nosotros"== $pagina)?'active-menu':''; ?>"><a href="nosotros">NOSOTROS </a></li> 
 						
-					<?php 
+					<?php /*
 						$categ=executesql("select * from categorias where estado_idestado=1 order by orden desc");
 						if(!empty($categ)){ ?>
 							<li class="submenu sub_personal <?php echo $active; ?>">
@@ -135,7 +185,8 @@ if(empty($_SESSION["suscritos"]["id_suscrito"])){ ?>
 									<?php	} ?>						
 								</ul>
 							</li>	
-					<?php	} ?>	
+					<?php	} 
+					*/ ?>	
 						
 						<li class="<?php echo ("blog"== $pagina)?'active-menu':''; ?>"><a href="blog">BLOG </a></li>  
 						<li class="<?php echo ("contacto"== $pagina)?'active-menu':''; ?>"><a href="contacto">CONTÁCTENOS </a></li>  
